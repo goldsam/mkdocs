bcf43afc7fb1b3262cf1848066c14153946a07cf
Sam Goldmann <sam.goldmann@gmail.com>
2022-02-25 09:54:01 -0500

    cleanup entrypoint script

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -1,25 +1,14 @@
 #!/bin/sh
 
+#
+# Start local plantuml server and wait for it to be ready.
+#
+
 max_iterations=20
 wait_seconds=0.5
 http_endpoint="$PLANTUML_SERVER"
 
-if [ "$1" = 'serve' ]; then
-  dev_addr_set=false
-  for item in "${@:2}"; do
-    if [ "$item" = '-a' ] || [ "$item" = '--dev-addr' ]; then
-      dev_addr_set=true
-    fi
-  done
-
-  if [ "$dev_addr_set" = 'false' ]; then
-    set -- "$@" '-a' '0.0.0.0:8000'
-  fi
-
-fi
-
 java -jar /app/plantuml.jar -Dplantuml.include.path=/docs -picoweb:8001 &
-
 printf 'waiting for plauntuml server to be ready'
 
 i=0
@@ -35,12 +24,33 @@ while true; do
     break
   fi
 
-  if [ "$i" -ge "$max_iterations" ]; then
+ if [ "$i" -ge "$max_iterations" ]; then
+    # TODO: Write to stderr?
     printf 'Error: plauntuml server did not respond in a timely manner.'
     exit 1
   fi
 
   sleep $wait_seconds
 done
+
+
+#
+# Foward arguments to mkdocs with special handling for the 'serve' command:
+# If the -a/--dev-addr arg is not specified, then set it with a value of 0.0.0.0:8000.
+#
+
+if [ "$1" = 'serve' ]; then
+  dev_addr_set=false
+  for item in "${@:2}"; do
+    if [ "$item" = '-a' ] || [ "$item" = '--dev-addr' ]; then
+      dev_addr_set=true
+    fi
+  done
+
+  if [ "$dev_addr_set" = 'false' ]; then
+    set -- "$@" '-a' '0.0.0.0:8000'
+  fi
+
+fi
 
 mkdocs "$@"
